59561b5fd95cf7a3c49fc573b6b1b55495378295
han <walcoorperation@gmail.com>
2023-06-13 03:13:05 +0800

    Update app with private repo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Logo } from "walcron-microfrontend-shared";
+import { Logo } from "@yoonghan/walcron-microfrontend-shared";
 
 function App() {
   const [count, setCount] = useState(0);
