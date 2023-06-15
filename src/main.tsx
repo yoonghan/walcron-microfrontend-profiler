dd66efe7e5df4d987ac005c597a3f3680d9aa043
han <walcoorperation@gmail.com>
2023-06-15 18:39:03 +0800

    Add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import reportWebVitals from "./webvital/reportWebVitals";
 import consoleReportHandler from "./webvital/consoleReportHandler";
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
