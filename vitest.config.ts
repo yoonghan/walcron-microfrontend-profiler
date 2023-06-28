153d0b0cc4470a9aa4bcc91f8d6aff8043f76b69
han <walcoorperation@gmail.com>
2023-06-29 07:26:50 +0800

    Split test cases

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import { baseConfig } from "./vite.config";
+
+export default defineConfig({
+  ...baseConfig,
+  test: {
+    global: true,
+    environment: "jsdom",
+    setupFiles: "./test-setup.js",
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "cobertura"],
+      threshold: {
+        global: {
+          branches: 100,
+          functions: 100,
+          lines: 100,
+          statements: 100,
+        },
+      },
+    },
+  },
+});
