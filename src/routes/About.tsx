9ee201da42b5b76f326288f9491a6a43255b3907
han <walcoorperation@gmail.com>
2023-07-19 07:35:35 +0800

    Fix about alignment

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,17 +1,21 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, Grid } from "@mui/material";
 import packageJson from "../../package.json";
 
 const About = () => {
   return (
     <Container component="main" maxWidth="xs">
-      <Typography component="h1">About</Typography>
-      <Typography component="p">
-        This site is used for user up-keep and authentication.
-      </Typography>
+      <Grid container justifyContent="center">
+        <Typography component="h1" variant="h1">
+          About
+        </Typography>
+        <Typography component="p">
+          This site is used for user up-keep and authentication.
+        </Typography>
 
-      <Typography align="center">
-        Version<span>{packageJson.version}</span>
-      </Typography>
+        <Typography align="center">
+          Version <span>{packageJson.version}</span>
+        </Typography>
+      </Grid>
     </Container>
   );
 };
